4ceaaecf314bc46d2a65e1a25d3776a426714884
ys19500 <ys215@st-andrews.ac.uk>
2025-05-12 22:49:39 +0530

    install_chrome

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+# Install dependencies without root
 apt-get update
-apt-get install -y wget gnupg unzip
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y wget curl gnupg2 ca-certificates libx11-xcb1 libxcomposite1 libxrandr2 xdg-utils
+
+# Install Chrome in user space (no root required)
+CHROME_VERSION="google-chrome-stable_current_amd64.deb"
+wget https://dl.google.com/linux/direct/$CHROME_VERSION
+dpkg -i $CHROME_VERSION || apt-get install -f -y
+
+# Clean up
+rm $CHROME_VERSION
+
+# Check installation
+google-chrome-stable --version
