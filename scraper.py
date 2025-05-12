38f3f31cbf6b570764b5494c73233b7fbc8ed4c7
ys19500 <ys215@st-andrews.ac.uk>
2025-05-12 23:45:49 +0530

    render updates minor

--- scraper.py
+++ scraper.py
@@ -11,8 +11,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+options = Options()
 def get_driver():
-    options = Options()
+    
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
