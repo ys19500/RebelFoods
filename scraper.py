350cc9454d6cfa312517946ca2db87c2918a67b6
ys19500 <ys215@st-andrews.ac.uk>
2025-05-12 22:18:08 +0530

    bug3

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
-options.add_argument('--headless') 
+#options.add_argument('--headless') 
 options.add_argument('--disable-gpu')  
 options.add_argument('--no-sandbox')  
 options.add_argument('--disable-dev-shm-usage')  
