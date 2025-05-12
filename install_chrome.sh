3efae4b28f2d88fff5b0b2f888a8c3a530d3f4b5
ys19500 <ys215@st-andrews.ac.uk>
2025-05-12 22:40:55 +0530

    test

---
+++ install_chrome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update
+apt-get install -y wget gnupg unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
